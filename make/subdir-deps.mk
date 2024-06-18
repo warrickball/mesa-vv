551ab735531c506df34834b7b80ce6356784db8a
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2024-06-18 16:25:24 +1000

    build: fix make not working in subfolder

--- make/subdir-deps.mk
+++ make/subdir-deps.mk
@@ -1,13 +1,12 @@
-define pc
-PKG_CONFIG_PATH=$(addprefix ../,$(addsuffix /build/lib/pkgconfig,$(1))):$(PKG_CONFIG_PATH)
-endef
-
 define build_and_test
-$(BUILD_SUBDIR) $(1) $(call pc,$(2))
-$(CHECK_SUBDIR) $(1) $(call pc,$(2))
+$(BUILD_SUBDIR) $(1)
+$(CHECK_SUBDIR) $(1)
 endef
 
-$(BUILD_DIR)/depend: $(addsuffix /Makefile,$(SUBDIRS)) make/gen-folder-deps | $(BUILD_DIR)/
-	make/gen-folder-deps "$(MAKE)" $(SUBDIRS) > $(BUILD_DIR)/depend
+NODEPS := $(filter clean,$(MAKECMDGOALS))
 
+ifeq ($(NODEPS),)
+$(BUILD_DIR)/depend: $(addsuffix /Makefile,$(SUBDIRS)) make/gen-folder-deps | $(BUILD_DIR)
+	make/gen-folder-deps "$(MAKE)" $(SUBDIRS) > $(BUILD_DIR)/depend
 include $(BUILD_DIR)/depend
+endif
