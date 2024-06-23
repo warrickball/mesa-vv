382d6deef3f629301a7af1a7cbdd5d589ecbd69b
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2024-06-23 21:38:48 +1000

    build: remove last remaining grouped target

--- make/check.mk
+++ make/check.mk
@@ -6,7 +6,7 @@ $(CHECKER): $(ALL_DEPS) $(OBJS_CHECK) | $(BUILD_DIR)/bin/
 	$(EXECUTABLE) -o $(CHECKER) $(OBJS_CHECK) $(call pkg-config,--libs --static,mesa-$(MODULE_NAME))
 
 ifneq ($(OBJS_CHECK),)
-  $(CHECK_RESULTS) &: $(CHECKER) | $(CHECK_RESULT_DIR)/
+  $(CHECK_RESULTS) : $(CHECKER) | $(CHECK_RESULT_DIR)/
 	pushd test > /dev/null ; ../$(CHECKER) > ../$(CHECK_RESULTS); popd > /dev/null
 	diff -b $(CHECK_RESULTS) $(CHECK_RESULTS_GOLDEN)
 
